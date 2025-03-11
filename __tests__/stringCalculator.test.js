7548ed623838fcf2c400ee976111fa03f90a9f07
Ashish Laturiya <ashish.laturiya@xenabler.digital>
2025-03-12 01:12:20 +0530

    Write the implementation and test case with new line between numbers

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -16,3 +16,7 @@ test('two numbers return their sum', () => {
 test('multiple numbers return their sum', () => {
     expect(add("1,5,6")).toBe(12)
 })
+
+test('New line between the numbers is also a valid seperator', () => {
+    expect(add("1\n2, 4")).toBe(7)
+})
